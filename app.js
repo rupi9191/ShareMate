20d55ee4d72fe2100038d0e776a42615ebc5588c
rupi9191 <rupi.9191@gmail.com>
2015-12-13 17:29:35 +0530

    api test

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.get('/', function(req, res) {
     // ejs render automatically looks in the views folder
     res.render('index');
 });
+app.get('/users', function(req, res) {
+   res.json({ message: 'hooray! welcome to our api!' }); 
+});
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
