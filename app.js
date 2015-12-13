84786e2b82e3b77df88d377b194347aaaf95b12b
rupi9191 <rupi.9191@gmail.com>
2015-12-13 20:06:34 +0530

    mongo conf

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+//var mongoose = require('mongoose');
+
 var mongodb;
-//mongodb://localhost:27017/sample1
-//mongodb://rupesh:rupesh1@ds027335.mongolab.com:27335/heroku_1x6mqtdt
-// MongoClient.connect('mongodb://rupesh:rupesh1@ds027335.mongolab.com:27335/heroku_1x6mqtdt', function(err, db) {
-  // if (err) {
-    // throw err;
-  // }
-  // mongodb = db;
-// });
+MongoClient.connect('mongodb://rupesh:rupesh@ds027335.mongolab.com:27335/heroku_1x6mqtdt', function(err, db) {
+  if (err) {
+    throw err;
+  }
+  mongodb = db;
+});
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 4000;
@@ -22,20 +22,21 @@ app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
-
     // ejs render automatically looks in the views folder
     res.render('index');
 });
 app.get('/users', function(req, res) {
-   res.json({ message: 'hooray! welcome to our api!' }); 
-    // mongodb.collection('usercollection').find().toArray(function(err, result) {
-    // if (err) {
-      // throw err;
-    // }
-    // res.json({ users: result}); 
-  // });
+  // res.json({ message: 'hooray! welcome to our api!' }); 
+    mongodb.collection('usercollection').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    res.json({ users: result}); 
+  });
 });
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+
